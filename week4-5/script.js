ba542f127e9de81f31140928be6f510ab88aa1dc
Shahla <147069716+ShahlaAbdul@users.noreply.github.com>
2023-10-30 08:13:08 +0400

    Update script.js

--- week4-5/script.js
+++ week4-5/script.js
@@ -47,3 +47,24 @@
 // }
 // console.log(myfunction2(array));
 
+ // ?---------TASSK\4------------------------------
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+// let array1 = []; let array2 = []; let hasil1 = 1; let hasil2 = 1; let hasil3 = 1;
+
+// function myfunction2(array) {
+//     for (let i = 0; i < array; i++) {
+//         if (array[i] % 2 !== 0) {
+//             array1.push(array[i]);
+//             hasil1 = hasil1 * array1[i]
+//         }
+//         if (array[i] % 2 !== 0) {
+//             array2.push(array[i]);
+//             hasil2 = hasil2 * array2[i]
+//         }
+//          hasil3=hasil1*hasil2
+//     }
+//     return(hasil3);
+// }
+// console.log(myfunction2());
+
