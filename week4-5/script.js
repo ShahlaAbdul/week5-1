ac0a9a8156a88d5f819546a8d28d42d24fbd6e90
Shahla <147069716+ShahlaAbdul@users.noreply.github.com>
2023-10-30 08:00:44 +0400

    Update script.js

--- week4-5/script.js
+++ week4-5/script.js
@@ -26,6 +26,24 @@
 //         }  
 //     }
 //     return(array2);
+
+
+
+ // ?---------TASSK3------------------------------
+
+// const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+// let array2 = []
+// let sum = 0;
+
+// function myfunction2(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % 2 !== 0) {
+//             array2.push(array[i]);
+//         }
+//     }
+//     return(array2);
+// }
+// console.log(myfunction2(array));
 // }
 // console.log(myfunction2(array));
 
